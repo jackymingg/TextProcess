c3e50a48e337ab4394f133f8930e71c749084211
jackymingg <43320083+jackymingg@users.noreply.github.com>
2025-06-17 11:37:35 +0800

    Add files via upload

--- app/Http/Requests/TextProcessRequest.php
+++ app/Http/Requests/TextProcessRequest.php
@@ -6,19 +6,7 @@ use Illuminate\Foundation\Http\FormRequest;
 
 class TextProcessRequest extends FormRequest
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     */
-    public function authorize(): bool
-    {
-        return true;
-    }
 
-    /**
-     * Get the validation rules that apply to the request.
-     *
-     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array|string>
-     */
     public function rules(): array
     {
         return [
@@ -28,9 +16,6 @@ class TextProcessRequest extends FormRequest
         ];
     }
 
-    /**
-     * Get custom messages for validator errors.
-     */
     public function messages(): array
     {
         return [
